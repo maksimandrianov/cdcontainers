7795cf113332d129802cddf96f9bbb09c745abe2
maksimandrianov <maksimandrianov1@gmail.com>
2017-11-06 06:30:59 +0300

    Added cdc_stackl_t

--- include/cdcontainers/stackl.h
+++ include/cdcontainers/stackl.h
@@ -1,6 +1,29 @@
 #ifndef CDCONTAINERS_INCLUDE_CDCONTAINERS_STACKL_H
 #define CDCONTAINERS_INCLUDE_CDCONTAINERS_STACKL_H
 
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <cdcontainers/status.h>
+#include <cdcontainers/common.h>
 
+typedef struct cdc_stack_list cdc_stackl_t;
+
+enum cdc_stat cdc_stackl_ctor (cdc_stackl_t **s, cdc_free_func_t func);
+enum cdc_stat cdc_stackl_ctorl(cdc_stackl_t **s, cdc_free_func_t func, ...);
+enum cdc_stat cdc_stackl_ctorv(cdc_stackl_t **s, cdc_free_func_t func, va_list args);
+void          cdc_stackl_dtor (cdc_stackl_t *s);
+
+// Element access
+void *        cdc_stackl_top  (cdc_stackl_t *s);
+
+// Capacity
+bool          cdc_stackl_empty(cdc_stackl_t *s);
+size_t        cdc_stackl_size (cdc_stackl_t *s);
+
+// Modifiers
+enum cdc_stat cdc_stackl_push (cdc_stackl_t *s, void *elem);
+enum cdc_stat cdc_stackl_pop  (cdc_stackl_t *s);
+void          cdc_stackl_swap (cdc_stackl_t *a, cdc_stackl_t *b);
 
 #endif  // CDCONTAINERS_INCLUDE_CDCONTAINERS_STACKL_H
--- src/stackl.c
+++ src/stackl.c
@@ -1,1 +1,112 @@
 #include "cdcontainers/stackl.h"
+
+#include <assert.h>
+#include "cdcontainers/list.h"
+#include "cdcontainers/common.h"
+
+struct cdc_stack_list {
+        cdc_list_t *list;
+};
+
+enum cdc_stat cdc_stackl_ctor(cdc_stackl_t **s, cdc_free_func_t func)
+{
+        cdc_stackl_t *tmp;
+        enum cdc_stat ret;
+
+        tmp = (cdc_stackl_t *)malloc(sizeof(cdc_stackl_t));
+        if (!tmp)
+                return CDC_STATUS_BAD_ALLOC;
+
+         ret = cdc_list_ctor(&tmp->list, func);
+         if (ret != CDC_STATUS_OK) {
+                 free(tmp);
+                 return ret;
+         }
+
+         *s = tmp;
+
+         return CDC_STATUS_OK;
+}
+
+enum cdc_stat cdc_stackl_ctorl(cdc_stackl_t **s, cdc_free_func_t func, ...)
+{
+        assert(s != NULL);
+
+        enum cdc_stat ret;
+        va_list args;
+
+        va_start(args, func);
+        ret = cdc_stackl_ctorv(s, func, args);
+        va_end(args);
+
+        return ret;
+}
+
+enum cdc_stat cdc_stackl_ctorv(cdc_stackl_t **s, cdc_free_func_t func,
+                               va_list args)
+{
+        assert(s != NULL);
+
+        enum cdc_stat ret;
+
+        ret = cdc_stackl_ctor(s, func);
+        if (ret != CDC_STATUS_OK)
+                return ret;
+
+        ret = cdc_list_ctorv(&(*s)->list, func, args);
+
+        return ret;
+}
+
+void cdc_stackl_dtor(cdc_stackl_t *s)
+{
+        assert(s != NULL);
+
+        cdc_list_dtor(s->list);
+        free(s);
+}
+
+void *cdc_stackl_top(cdc_stackl_t *s)
+{
+        assert(s != NULL);
+        assert(cdc_list_size(s->list) > 0);
+
+        return cdc_list_back(s->list);
+}
+
+bool cdc_stackl_empty(cdc_stackl_t *s)
+{
+      assert(s != NULL);
+
+      return cdc_list_empty(s->list);
+}
+
+size_t cdc_stackl_size(cdc_stackl_t *s)
+{
+        assert(s != NULL);
+
+        return cdc_list_size(s->list);
+}
+
+enum cdc_stat cdc_stackl_push(cdc_stackl_t *s, void *elem)
+{
+        assert(s != NULL);
+
+        return cdc_list_push_back(s->list, elem);
+}
+
+enum cdc_stat cdc_stackl_pop(cdc_stackl_t *s)
+{
+        assert(s != NULL);
+        assert(cdc_list_size(s->list) > 0);
+
+        return cdc_list_pop_back(s->list);
+}
+
+void cdc_stackl_swap(cdc_stackl_t *a, cdc_stackl_t *b)
+{
+        assert(a != NULL);
+        assert(b != NULL);
+
+        CDC_SWAP(cdc_list_t *, a->list, b->list);
+}
