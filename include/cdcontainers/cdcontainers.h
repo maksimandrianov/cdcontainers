6a2effd72b0100bc8cdc1d05c2f4ab412b2634d6
maksimandrianov <maksimandrianov1@gmail.com>
2017-11-16 04:19:27 +0300

    Renamed file

---
+++ include/cdcontainers/cdcontainers.h
@@ -0,0 +1,11 @@
+#ifndef CDCONTAINERS_INCLUDE_CDCONTAINERS_CDCONTAINERS_H
+#define CDCONTAINERS_INCLUDE_CDCONTAINERS_CDCONTAINERS_H
+
+#include <cdcontainers/vector.h>
+#include <cdcontainers/list.h>
+#include <cdcontainers/stack.h>
+#include <cdcontainers/queue.h>
+#include <cdcontainers/deque.h>
+#include <cdcontainers/priority-queue.h>
+
+#endif  // CDCONTAINERS_INCLUDE_CDCONTAINERS_CDCONTAINERS_H
