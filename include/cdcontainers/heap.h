30387cff956489a076ffebb679daf3fe5b731aea
maksimandrianov <maksimandrianov1@gmail.com>
2017-11-16 04:10:32 +0300

    Added short names

--- include/cdcontainers/heap.h
+++ include/cdcontainers/heap.h
@@ -27,5 +27,27 @@ enum cdc_stat cdc_heap_insert      (cdc_heap_t *h, void *key);
 void          cdc_heap_increase_key(cdc_heap_t *h, size_t i, void *key);
 void          cdc_heap_swap        (cdc_heap_t *a, cdc_heap_t *b);
 
+// Short names
+#ifdef CDC_USE_SHORT_NAMES
+typedef cdc_heap_t heap_t;
+
+#define heap_ctor(...)         cdc_heap_ctor(__VA_ARGS__)
+#define heap_ctorl(...)        cdc_heap_ctorl(__VA_ARGS__)
+#define heap_ctorv(...)        cdc_heap_ctorv(__VA_ARGS__)
+#define heap_dtor(...)         cdc_heap_dtor(__VA_ARGS__)
+
+// Element access
+#define heap_top(...)          cdc_heap_top(__VA_ARGS__)
+
+// Capacity
+#define heap_empty(...)        cdc_heap_empty(__VA_ARGS__)
+#define heap_size(...)         cdc_heap_size(__VA_ARGS__)
+
+// Modifiers
+#define heap_extract_top(...)  cdc_heap_extract_top(__VA_ARGS__)
+#define heap_insert(...)       cdc_heap_insert(__VA_ARGS__)
+#define heap_increase_key(...) cdc_heap_increase_key(__VA_ARGS__)
+#define heap_swap(...)         cdc_heap_swap(__VA_ARGS__)
+#endif
 
 #endif  // CDCONTAINERS_INCLUDE_CDCONTAINERS_HEAP_H
--- include/cdcontainers/priority-queue.h
+++ include/cdcontainers/priority-queue.h
@@ -26,4 +26,26 @@ enum cdc_stat cdc_priority_queue_push (cdc_priority_queue_t *q, void *elem);
 enum cdc_stat cdc_priority_queue_pop  (cdc_priority_queue_t *q);
 void          cdc_priority_queue_swap (cdc_priority_queue_t *a, cdc_priority_queue_t *b);
 
+// Short names
+#ifdef CDC_USE_SHORT_NAMES
+typedef cdc_priority_queue_t priority_queue_t;
+
+#define priority_queue_ctor(...)   cdc_priority_queue_ctor(__VA_ARGS__)
+#define priority_queue_ctorl(...)  cdc_priority_queue_ctorl(__VA_ARGS__)
+#define priority_queue_ctorv(...)  cdc_priority_queue_ctorv(__VA_ARGS__)
+#define priority_queue_dtor(...)   cdc_priority_queue_dtor(__VA_ARGS__)
+
+// Element access
+#define priority_queue_top(...)    cdc_priority_queue_top(__VA_ARGS__)
+
+// Capacity
+#define priority_queue_empty(...)  cdc_priority_queue_empty(__VA_ARGS__)
+#define priority_queue_size(...)   cdc_priority_queue_size(__VA_ARGS__)
+
+// Modifiers
+#define priority_queue_push(...)   cdc_priority_queue_push(__VA_ARGS__)
+#define priority_queue_pop(...)    cdc_priority_queue_pop(__VA_ARGS__)
+#define priority_queue_swap(...)   cdc_priority_queue_swap(__VA_ARGS__)
+#endif
+
 #endif  // CDCONTAINERS_INCLUDE_CDCONTAINERS_PRIORITY_QUEUE_H
