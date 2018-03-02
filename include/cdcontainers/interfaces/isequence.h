821cbc7b8bb2806aaa090a2f2fa542e31d2dc95e
Maksim Andrianov <maksimandrianov1@yandex.ru>
2018-03-03 02:19:09 +0300

    Rewritten using sequence container interface

--- include/cdcontainers/interfaces/isequence.h
+++ include/cdcontainers/interfaces/isequence.h
@@ -1,5 +1,5 @@
 // The MIT License (MIT)
-// Copyright (c) 2017 Maksim Andrianov
+// Copyright (c) 2018 Maksim Andrianov
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to
