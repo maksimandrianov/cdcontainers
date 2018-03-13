6edbe6bcd5ad5e0a9f83bca3e012399330e3fd78
Maksim Andrianov <maksimandrianov1@yandex.ru>
2018-03-13 07:30:33 +0300

    Added utilities tests

---
+++ tests/test-common.c
@@ -0,0 +1,42 @@
+// The MIT License (MIT)
+// Copyright (c) 2018 Maksim Andrianov
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+#include "test-common.h"
+
+#include <CUnit/Basic.h>
+#include "cdcontainers/casts.h"
+
+void test_ptr_float_cast()
+{
+#ifdef CDC_PTR_FLOAT_CAST
+        float value = 123456.123456;
+
+        CU_ASSERT(CDC_PTR_TO_FLOAT(CDC_FLOAT_TO_PTR(value)) == value);
+#endif
+}
+
+void test_ptr_double_cast()
+{
+#ifdef CDC_PTR_DOUBLE_CAST
+        double value = 123456.123456;
+
+        CU_ASSERT(CDC_PTR_TO_DOUBLE(CDC_DOUBLE_TO_PTR(value)) == value);
+#endif
+}
