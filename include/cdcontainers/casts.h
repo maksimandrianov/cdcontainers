06986007bcfe56a583c39ca77e553d2b0a6fe94e
Maksim Andrianov <maksimandrianov1@yandex.ru>
2018-03-13 07:29:23 +0300

    Added utilities

---
+++ include/cdcontainers/casts.h
@@ -0,0 +1,118 @@
+// The MIT License (MIT)
+// Copyright (c) 2018 Maksim Andrianov
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+#ifndef CDCONTAINERS_INCLUDE_CDCONTAINERS_CASTS_H
+#define CDCONTAINERS_INCLUDE_CDCONTAINERS_CASTS_H
+
+#include <stdint.h>
+#include <float.h>
+#include <limits.h>
+#include <stddef.h>
+#include <cdcontainers/common.h>
+
+
+#define CDC_PTR_TO_CHAR(p) ((char) (intptr_t) (p))
+#define CDC_CHAR_TO_PTR(s) ((void *) (char) (s))
+
+#define CDC_PTR_TO_SCHAR(p) ((signed char) (intptr_t) (p))
+#define CDC_SCHAR_TO_PTR(s) ((void *) (signed char) (s))
+
+#define CDC_PTR_TO_UCHAR(p) ((unsigned char) (uintptr_t) (p))
+#define CDC_UCHAR_TO_PTR(s) ((void *) (unsigned char) (s))
+
+#define CDC_PTR_TO_SHORT(p) ((short) (intptr_t) (p))
+#define CDC_SHORT_TO_PTR(s) ((void *) (short) (s))
+
+#define CDC_PTR_TO_USHORT(p) ((unsigned short) (uintptr_t) (p))
+#define CDC_USHORT_TO_PTR(s) ((void *) (unsigned short) (s))
+
+#define CDC_PTR_TO_INT(p) ((int) (intptr_t) (p))
+#define CDC_INT_TO_PTR(s) ((void *) (int) (s))
+
+#define CDC_PTR_TO_UINT(p) ((unsigned int) (uintptr_t) (p))
+#define CDC_UINT_TO_PTR(s) ((void *) (unsigned int) (s))
+
+#define CDC_PTR_TO_SIZE(p) ((size_t) (uintptr_t) (p))
+#define CDC_SIZE_TO_PTR(s) ((void *) (size_t) (s))
+
+#if UINTPTR_MAX >= ULONG_MAX
+# define CDC_PTR_TO_LONG(p)	((long) (intptr_t) (p))
+# define CDC_LONG_TO_PTR(s)	((void *) (long) (s))
+# define CDC_PTR_LONG_CAST
+
+# define CDC_PTR_TO_ULONG(p) ((unsigned long) (uintptr_t) (p))
+# define CDC_ULONG_TO_PTR(s) ((void *) (unsigned long) (s))
+# define CDC_PTR_ULONG_CAST
+#endif
+
+#if (defined(__GNUC__)) && \
+        defined(__SIZEOF_POINTER__)
+# if defined(__SIZEOF_FLOAT__) && (__SIZEOF_POINTER__ >= __SIZEOF_FLOAT__)
+union cdc_union_float_ptr {
+        void *ptr;
+        float flt;
+};
+
+static inline void *cdc_float_to_ptr(float flt)
+{
+        union cdc_union_float_ptr u;
+        u.flt = flt;
+        return u.ptr;
+}
+
+static inline float cdc_ptr_to_float(void *ptr)
+{
+        union cdc_union_float_ptr u;
+        u.ptr = ptr;
+        return u.flt;
+}
+
+# define CDC_PTR_TO_FLOAT(p) cdc_ptr_to_float(p)
+# define CDC_FLOAT_TO_PTR(s) cdc_float_to_ptr(s)
+# define CDC_PTR_FLOAT_CAST
+# endif
+
+# if defined(__SIZEOF_DOUBLE__) && (__SIZEOF_POINTER__ >= __SIZEOF_DOUBLE__)
+union cdc_union_double_ptr {
+        void *ptr;
+        double dbl;
+};
+
+static inline void *cdc_double_to_ptr(double dbl)
+{
+        union cdc_union_double_ptr u;
+        u.dbl = dbl;
+        return u.ptr;
+}
+
+static inline double cdc_ptr_to_double(void *ptr)
+{
+        union cdc_union_double_ptr u;
+        u.ptr = ptr;
+        return u.dbl;
+}
+
+# define CDC_PTR_TO_DOUBLE(p) cdc_ptr_to_double(p)
+# define CDC_DOUBLE_TO_PTR(s) cdc_double_to_ptr(s)
+# define CDC_PTR_DOUBLE_CAST
+# endif
+#endif
+
+#endif  // CDCONTAINERS_INCLUDE_CDCONTAINERS_CASTS_H
---
+++ include/cdcontainers/hash.h
@@ -0,0 +1,164 @@
+// The MIT License (MIT)
+// Copyright (c) 2018 Maksim Andrianov
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+/**
+  * @file
+  * @author Maksim Andrianov <maksimandrianov1@yandex.ru>
+  * @brief This file contains different utilities for hashing.
+  * The ideas of algorithms were borrowed from the boost library.
+  * http://www.boost.org/doc/libs/1_64_0/boost/functional/hash/hash.hpp
+  * http://www.boost.org/doc/libs/1_64_0/boost/functional/hash/detail/hash_float.hpp
+  */
+#ifndef CDCONTAINERS_INCLUDE_CDCONTAINERS_HASH_H
+#define CDCONTAINERS_INCLUDE_CDCONTAINERS_HASH_H
+
+#include <stddef.h>
+#include <limits.h>
+#include <float.h>
+#include <string.h>
+#include <cdcontainers/casts.h>
+
+typedef size_t (*cdc_hash_fn_t)(void *);
+
+#define CDC_DIGITS_CHAR (CHAR_BIT - (CHAR_MIN < 0))
+#define CDC_DIGITS_SCHAR (CHAR_BIT - 1)
+#define CDC_DIGITS_UCHAR (CHAR_BIT)
+#define CDC_DIGITS_SHORT (CHAR_BIT * sizeof(short) - 1)
+#define CDC_DIGITS_USHORT (CHAR_BIT * sizeof(short))
+#define CDC_DIGITS_INT (CHAR_BIT * sizeof(int) - 1)
+#define CDC_DIGITS_UINT (CHAR_BIT * sizeof(int))
+#define CDC_DIGITS_LONG (CHAR_BIT * sizeof(long) - 1)
+#define CDC_DIGITS_ULONG (CHAR_BIT * sizeof(long))
+#define CDC_DIGITS_SIZE (CHAR_BIT * sizeof(size_t))
+
+#define MAKE_SIGNED_HASH(T, DIGITS, NAME) \
+        static inline size_t cdc_hash_##NAME(T val) \
+        { \
+                const unsigned int size_t_bits = CDC_DIGITS_SIZE; \
+                const int length = (DIGITS - 1) / (int)(size_t_bits); \
+                unsigned int i; \
+                size_t seed = 0; \
+                T positive = val < 0 ? -1 - val : val; \
+                for (i = length * size_t_bits; i > 0; i -= size_t_bits) \
+                        seed ^= (size_t) (positive >> i) + (seed << 6) + (seed >> 2); \
+                seed ^= (size_t) val + (seed << 6) + (seed >> 2); \
+                return seed; \
+        } \
+
+#define MAKE_UNSIGNED_HASH(T, DIGITS, NAME) \
+        static inline size_t cdc_hash_##NAME(T val) \
+        { \
+                const unsigned int size_t_bits = CDC_DIGITS_SIZE; \
+                const int length = (DIGITS - 1) / (int)(size_t_bits); \
+                unsigned int i; \
+                size_t seed = 0; \
+                for (i = length * size_t_bits; i > 0; i -= size_t_bits) \
+                        seed ^= (size_t) (seed >> i) + (seed << 6) + (seed >> 2); \
+                seed ^= (size_t) val + (seed << 6) + (seed >> 2); \
+                return seed; \
+        } \
+
+static inline size_t cdc_hash_float_combine(size_t seed, size_t value)
+{
+        seed ^= value + (seed << 6) + (seed >> 2);
+        return seed;
+}
+
+static inline size_t cdc_hash_binary(char* ptr, size_t length)
+{
+        size_t seed = 0;
+
+        if (length >= sizeof(size_t)) {
+                memcpy(&seed, ptr, sizeof(size_t));
+                length -= sizeof(size_t);
+                ptr += sizeof(size_t);
+
+                while(length >= sizeof(size_t)) {
+                        size_t buffer = 0;
+                        memcpy(&buffer, ptr, sizeof(size_t));
+                        seed = cdc_hash_float_combine(seed, buffer);
+                        length -= sizeof(size_t);
+                        ptr += sizeof(size_t);
+                }
+        }
+
+        if (length > 0) {
+                size_t buffer = 0;
+                memcpy(&buffer, ptr, length);
+                seed = cdc_hash_float_combine(seed, buffer);
+        }
+
+        return seed;
+}
+
+#define MAKE_FLOAT_HASH(T, NAME) \
+        static inline size_t cdc_hash_##NAME(T val) \
+        { \
+                return cdc_hash_binary((char*)&val, sizeof(T)); \
+        } \
+
+MAKE_SIGNED_HASH(signed char, CDC_DIGITS_SCHAR, schar)
+MAKE_SIGNED_HASH(short, CDC_DIGITS_SHORT, short)
+MAKE_SIGNED_HASH(int, CDC_DIGITS_INT, int)
+MAKE_SIGNED_HASH(long, CDC_DIGITS_LONG, long)
+
+MAKE_FLOAT_HASH(float, float)
+MAKE_FLOAT_HASH(double, double)
+MAKE_FLOAT_HASH(long double, ldouble)
+
+MAKE_UNSIGNED_HASH(unsigned char, CDC_DIGITS_UCHAR, uchar)
+MAKE_UNSIGNED_HASH(unsigned short, CDC_DIGITS_USHORT, ushort)
+MAKE_UNSIGNED_HASH(unsigned int, CDC_DIGITS_UINT, uint)
+MAKE_UNSIGNED_HASH(unsigned long, CDC_DIGITS_ULONG, ulong)
+
+#if CHAR_MIN < 0
+        MAKE_SIGNED_HASH(char, CDC_DIGITS_CHAR, char)
+#else
+        MAKE_UNSIGNED_HASH(char, CDC_DIGITS_CHAR, char)
+#endif
+
+#define MAKE_POINTER_DATA_HASH(T, NAME, CAST_FUNC) \
+        static inline size_t cdc_pdhash_##NAME(void *val) \
+        { \
+                T t = CAST_FUNC(val); \
+                return cdc_hash_##NAME(t); \
+        } \
+
+
+MAKE_POINTER_DATA_HASH(char, char, CDC_PTR_TO_CHAR)
+MAKE_POINTER_DATA_HASH(signed char, schar, CDC_PTR_TO_SCHAR)
+MAKE_POINTER_DATA_HASH(unsigned char, uchar, CDC_PTR_TO_UCHAR)
+MAKE_POINTER_DATA_HASH(short, short, CDC_PTR_TO_SHORT)
+MAKE_POINTER_DATA_HASH(unsigned short, ushort, CDC_PTR_TO_USHORT)
+MAKE_POINTER_DATA_HASH(int, int, CDC_PTR_TO_INT)
+MAKE_POINTER_DATA_HASH(unsigned int, uint, CDC_PTR_TO_UINT)
+
+#ifdef CDC_PTR_LONG_CAST
+        MAKE_POINTER_DATA_HASH(long, long, CDC_PTR_TO_LONG)
+        MAKE_POINTER_DATA_HASH(unsigned long, ulong, CDC_PTR_TO_ULONG)
+#endif
+#ifdef CDC_PTR_FLOAT_CAST
+        MAKE_POINTER_DATA_HASH(float, float, CDC_PTR_TO_FLOAT)
+#endif
+#ifdef CDC_PTR_DOUBLE_CAST
+        MAKE_POINTER_DATA_HASH(double, double, CDC_PTR_TO_DOUBLE)
+#endif
+
+#endif  // CDCONTAINERS_INCLUDE_CDCONTAINERS_HASH_H
